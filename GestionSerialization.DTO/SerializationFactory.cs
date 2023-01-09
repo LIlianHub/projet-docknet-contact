199f0e8242d42c1bb1c4b99c775056cfa216ffec
LIlianHub <73762102+LIlianHub@users.noreply.github.com>
2023-01-09 16:20:16 +0100

    Separation du projet

--- GestionSerialization.DTO/SerializationFactory.cs
+++ GestionSerialization.DTO/SerializationFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ballejos
 {
-    internal class SerializationFactory
+    public class SerializationFactory
     {
 
         // Retourne le sérialiseur désiré
